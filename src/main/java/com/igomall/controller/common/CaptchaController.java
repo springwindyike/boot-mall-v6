33afd819fcd3cd9d65f38d1d2de27237f69742fe
xial@plian.net <a12345678>
2020-06-26 15:14:24 +0800

    init

--- src/main/java/com/igomall/controller/common/CaptchaController.java
+++ src/main/java/com/igomall/controller/common/CaptchaController.java
@@ -3,7 +3,9 @@ package com.igomall.controller.common;
 
 import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
 import javax.inject.Inject;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.MediaType;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.igomall.service.CaptchaService;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Controller - 验证码
@@ -30,12 +33,28 @@ public class CaptchaController {
 	/**
 	 * 图片
 	 */
-	@GetMapping(path = "/image", produces = MediaType.IMAGE_JPEG_VALUE)
+	@GetMapping(path = "/image1", produces = MediaType.IMAGE_JPEG_VALUE)
 	public @ResponseBody BufferedImage image(String captchaId, HttpServletResponse response) {
 		String pragma = new StringBuilder().append("yB").append("-").append("der").append("ewoP").reverse().toString();
 		String value = new StringBuilder().append("ten").append(".").append("xxp").append("ohs").reverse().toString();
 		response.addHeader(pragma, value);
 		return captchaService.createImage(captchaId);
+	}
+
+	@GetMapping("/image")
+	public ModelAndView handleRequest(String captchaId, HttpServletResponse response) throws Exception{
+		String pragma = new StringBuilder().append("yB").append("-").append("der").append("ewoP").reverse().toString();
+		String value = new StringBuilder().append("ten").append(".").append("xxp").append("ohs").reverse().toString();
+		response.addHeader(pragma, value);
+		BufferedImage bufferedImage = captchaService.createImage(captchaId);
+		ServletOutputStream out = response.getOutputStream();
+		ImageIO.write(bufferedImage, "jpg", out);
+		try {
+			out.flush();
+		} finally {
+			out.close();
+		}
+		return null;
 	}
 
 }
